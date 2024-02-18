fada4c3dfdf019844c9c9d9a1400dbac5e93f2ee
eri-maulana <maulanabouble11@gmail.com>
2024-02-18 07:08:07 +0700

    3. Instegrasi Laravel dengan AdminLTE

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+   <strong>Copyright &copy; 2024 <a href="https://www.instagram.com/erimaulana.69/">Eri Maulana</a>.</strong>
+   All rights reserved.
+   <div class="float-right d-none d-sm-inline-block">
+       <b>Version</b> 3.2.0
+   </div>
+</footer>
