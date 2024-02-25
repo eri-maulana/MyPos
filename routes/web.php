8d8ede9af5966bfb9d70799bfa09e08a67d7a366
eri-maulana <maulanabouble11@gmail.com>
2024-02-25 17:18:46 +0700

    10. Membuat CRUD Produk

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\ProdukController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,6 +28,13 @@ Route::middleware([
 });
 
 Route::group(['middleware' => 'auth'], function () {
+    
+    // Kategori
     Route::get('/kategori/data', [KategoriController::class, 'data'])->name('kategori.data');
     Route::resource('/kategori', KategoriController::class);
+
+    // Produk
+    Route::get('/produk/data', [ProdukController::class, 'data'])->name('produk.data');
+    Route::resource('/produk', ProdukController::class);
+
 });
